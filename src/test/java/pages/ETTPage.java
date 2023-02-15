1df1da17fb8eb9a573bc71d6e45ce4c99f4bcc60
SONY <SONY@SONY-PC>
2023-02-15 18:43:46 +0530

    Pom class for ETT page

--- src/test/java/pages/ETTPage.java
+++ src/test/java/pages/ETTPage.java
@@ -1,5 +1,34 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class ETTPage {
+	
+	@FindBy(id="logoutLink")
+	private WebElement logoutLink;
+	
+	public ETTPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public boolean verifyHomePageIsDisplayed(WebDriverWait wait)
+	{
+		try
+		{
+			wait.until(ExpectedConditions.visibilityOf(logoutLink));
+			return true;
+		}
+		
+		catch(Exception e)
+		{
+			return false;
+		}
+	}
 
 }
