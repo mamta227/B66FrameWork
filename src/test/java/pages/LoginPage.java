42a6e443e1e778b3b043aed6944605507751f7c9
SONY <SONY@SONY-PC>
2023-02-16 20:05:29 +0530

    updated login page with errmsg

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 
 public class LoginPage {
 	
@@ -15,6 +18,9 @@ public class LoginPage {
 	
 	@FindBy(id="loginButton")
 	private WebElement loginBTN;
+	
+	@FindBy(xpath="//span[contains(text(),'invalid')]")
+	private WebElement errMsg;
 	
 	public LoginPage(WebDriver driver)
 	{
@@ -34,6 +40,23 @@ public class LoginPage {
 	public void clickLoginButton()
 	{
 		loginBTN.click();
+	}
+	
+	public boolean verifyErrormsgIsDisplayed(WebDriverWait wait)
+	{
+		try
+		{
+			wait.until(ExpectedConditions.visibilityOf(errMsg));
+			Reporter.log("Err msg is displayed, true");
+			return true;
+					
+		}
+		
+		catch(Exception e)
+		{
+			Reporter.log("Err msg is not displayed, true");
+			return false;
+		}
 	}
 
 }
